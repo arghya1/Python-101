9a626c65a2c8023d824099589a6137aae6f6e57b
arghya1 <basuarghyadroid99@gmail.com>
2023-05-05 16:52:20 +0530

    Day 2

--- python_basic.py
+++ python_basic.py
@@ -1,2 +1,27 @@
 # Print function
-print("Hello World")
+print("Hello World", "Hello", end='\n', sep='\t')
+
+# Input from user
+
+# name = input("Enter name of yourself:")
+# print("Your name by yourself is:", name)
+
+# Python LIst
+
+list1 = ["john", 102, "USA"]
+list2 = ["1", 2, 3, 4, 5, "6"]
+
+
+# print(type(list1))
+# print(type(list2))
+
+# Python Tuple
+tuple = ("Apple", "Mango", "Orange", "Banana")
+# tuple[2] = "Papaya"
+print(tuple)
+
+# Dictionaries
+students = {"Name": "John", "Age": 23, "Stipend": 25000, "Country": "USA"}
+# print(students)
+# print(type(students))
+
